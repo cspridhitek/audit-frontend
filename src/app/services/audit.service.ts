b9bbee6c6ff51cabfad0d9b2725e89c20be2e9ed
Nirmala Ramavath <nirmalaramavathofficial@gmail.com>
2025-03-14 17:23:01 +0530

    Added table with pagination

--- src/app/services/audit.service.ts
+++ src/app/services/audit.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { AuditLog } from '../audit-log/audit-log.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuditService {
-  private apiUrl = 'http://localhost:8080/api/audit';
+  private apiUrl = 'http://localhost:8081/api/audit?page=1&size=10&sortBy=timestamp&sortDirection=desc';
 
   constructor(private http: HttpClient) {}
 
   getAuditLogs(): Observable<AuditLog[]> {
-    return this.http.get<AuditLog[]>(this.apiUrl);
+    return this.http.get<{ content: AuditLog[] }>(this.apiUrl).pipe(
+      map(response => response.content) // Extracts logs from `content`
+    );
   }
 }
